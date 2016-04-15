72d3e5ee571253b453ab7ef116171c65d50df720
CHAYP <chay.promp@gmail.com>
2016-04-15 22:22:23 +0700

    leaves:rename getAll->getData,showAll->showData,showN->showTar ,init with mode['opt','per']

--- leaves.py
+++ leaves.py
@@ -1,26 +1,18 @@
 import math
 class leaves:
-    def __init__(self,n=2):
-        #print("__init leaves__");
+    def __init__(self,n=2,mode='opt'):
         self.n=n
         self.m=int(math.ceil(math.log(n,2)))
         #print("n {0} m {1}".format(self.n,self.m))
-
         self.tar=[[] for i in range(self.m+1)]
         self.tar[0]=[[i] for i in range(1,n+1)]
-        self.dat=[]
+        self.data=[]
 
-
-        self.gen()
-        #print("generate complete")
-        self.com(self.m+1,self.n,[],[])
-        #print("compond complete")
-
-
-        #self.permu(n,[0 for i in range(n+1)],[0 for i in range(n)])
-
-    def getAll(self):
-        return self.dat
+        if mode=='opt':
+            self.gen()
+            self.com(self.m+1,self.n,[],[])
+        elif mode=='per':
+            self.permu(n,[0 for i in range(n+1)],[0 for i in range(n)])
 
     def checkin(self,x,y):
         for i in x:
@@ -47,7 +39,7 @@ class leaves:
     def com(self,it,n,tmp,used):
         #print(it,n,tmp,used)
         if it==0:
-            self.dat.append(tmp)
+            self.data.append(tmp)
             return 1;
         if n&(1<<(it-1))>0:
             for i in self.tar[(it-1)]:
@@ -57,15 +49,9 @@ class leaves:
         else:
             self.com(it-1,n%(1<<it),tmp,used)
 
-    def showN(self,n):
-        [print (i) for i in self.tar[n]]
-
-    def showAll(self):
-        [print(i) for i in self.dat]
-
     def permu(self,n,used,tmp):
         if n==0:
-            self.dat.append(tmp[:])
+            self.data.append(tmp[:])
         for i in range(1,self.n+1):
             if used[i]==0:
                 used[i]=1
@@ -73,13 +59,15 @@ class leaves:
                 self.permu(n-1,used,tmp)
                 used[i]=0
 
+    def getData(self):
+        return self.data
 
+    def showTar(self,n):
+        [print (i) for i in self.tar[n]]
 
+    def showData(self):
+        [print(i) for i in self.data]
+        
 if __name__ == '__main__':
     leaf=leaves(5)
-    leaf.showAll()
-    #leaf.showN(1)
-    #leaf.show()
-    #while leaf.get()!=None:
-    #    print ('x')
-    #leaf.show()
+    leaf.showData()
